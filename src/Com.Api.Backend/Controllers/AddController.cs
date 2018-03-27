71239d5d74d52585526bff2395a7c1b9832438a9
Unknown <ojhawkins@gmail.com>
2018-03-28 10:19:01 +1100

    clean up

--- src/Com.Api.Backend/Controllers/AddController.cs
+++ src/Com.Api.Backend/Controllers/AddController.cs
@@ -5,30 +5,10 @@ namespace Com.Api.Backend.Controllers
     [Route("api/[controller]")]
     public class AddController : Controller
     {
-        // GET api/values
         [HttpGet]
         public int Get(int value)
         {
             return value + 5;
-        }
-
-        // POST api/values
-        [HttpPost]
-        public int Post(int value)
-        {
-            return value + 5;
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
--- src/Com.Api/Controllers/ValuesController.cs
+++ src/Com.Api/Controllers/ValuesController.cs
@@ -7,7 +7,6 @@ namespace Com.Api.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
         [HttpGet]
         public async Task<string> Get(int value)
         {
@@ -16,24 +15,6 @@ namespace Com.Api.Controllers
             var result = await httpClient.GetStringAsync($"api/add/{value}");
 
             return result;
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
