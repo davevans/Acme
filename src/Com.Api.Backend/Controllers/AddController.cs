7023b9a50f803cd0b70ccf47a97dca67c457a7ea
Charles Lowell <chlowe@microsoft.com>
2018-03-27 18:25:05 -0700

    add health check

--- src/Com.Api.Backend/Controllers/AddController.cs
+++ src/Com.Api.Backend/Controllers/AddController.cs
@@ -5,6 +5,8 @@ namespace Com.Api.Backend.Controllers
     [Route("api/[controller]")]
     public class AddController : Controller
     {
+        private static bool _isHealthy = true;
+
         [HttpGet("{value}")]
         public int Get(int value)
         {
